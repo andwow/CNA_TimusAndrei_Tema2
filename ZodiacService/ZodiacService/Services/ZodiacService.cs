221e30ae4b4ce11339145bac9fc066c7f17208dc
timus09 <andreitimus9@gmail.com>
2021-04-20 03:24:44 +0300

    GetSign

--- ZodiacService/ZodiacService/Services/ZodiacService.cs
+++ ZodiacService/ZodiacService/Services/ZodiacService.cs
@@ -11,6 +11,23 @@ namespace ZodiacService.Services
 {
     public class ZodiacService : Horoscope.HoroscopeBase
     {
-        
+        public static string GetSign(Zodiac zodiac)
+        {
+            var zodiacs = ZodiacOperations.GetAllZodiacs();
+
+            return (from variable in zodiacs
+
+                    let startMonth = int.Parse(variable.Item1.Date.Substring(0, 2))
+                    let startDay = int.Parse(variable.Item1.Date.Substring(3, 2))
+                    let endMonth = int.Parse(variable.Item2.Date.Substring(0, 2))
+                    let endDay = int.Parse(variable.Item2.Date.Substring(3, 2))
+
+                    let thisMonth = int.Parse(zodiac.Date.Substring(0, 2))
+                    let thisDay = int.Parse(zodiac.Date.Substring(3, 2))
+
+                    where thisMonth == startMonth && thisDay >= startDay || thisMonth == endMonth && thisDay <= endDay
+                    select variable.Item3).FirstOrDefault();
+        }
+
     }
 }
