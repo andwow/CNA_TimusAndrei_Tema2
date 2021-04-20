6a52a1ab1ac98f684701703d5bb7c3aa1efa3cc5
timus09 <andreitimus9@gmail.com>
2021-04-20 03:04:39 +0300

    Add project files.

---
+++ ZodiacClient/ZodiacClient/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+using ZodiacClient;
+
+namespace ZodiacClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Horoscope.HoroscopeClient(channel);
+
+            var cancellationToken = new CancellationTokenSource(Timeout.Infinite);
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Console.Write("Date: ");
+
+                var date = Console.ReadLine();
+                if(date.Length != 10) { return; }
+                if (!ValidateDate(date)) { return; }
+
+                var zodiacToBeAdded = new Zodiac()
+                { Date = date != null && date.Trim().Length > 0 ? date : "Invalid Date!" };
+                var response = await client.AddZodiacAsync(new AddZodiacRequest { Zodiac = zodiacToBeAdded });
+
+                switch (response.Status)
+                {
+                    case AddZodiacResponse.Types.Status.Success:
+                        Console.WriteLine($"Status: {response.Status}\nZodiacal Sign: {response.Sign}\n");
+                        break;
+                    case AddZodiacResponse.Types.Status.Error:
+                        Console.WriteLine($"Status: {response.Status}\nInvalid Date!\n");
+                        break;
+                    default:
+                        Console.WriteLine($"Status: {response.Status}\nSomething Wrong!\n");
+                        break;
+                }
+            }
+        }
+
+        public static bool ValidateDate(string date)
+        {
+            
+            var month = int.Parse(date.Substring(0, 2));
+            var day = int.Parse(date.Substring(3, 2));
+            var year = int.Parse(date.Substring(6, 4));
+            
+            try
+            {
+                var dateTime = new DateTime(year, month, day);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Invalid Date!");
+                return false;
+            }
+
+            var regex = new Regex("([0-9]{2})/([0-9]{2})/([0-9]{4})");
+            var match = regex.Match(date);
+
+            return match.Success;
+        }
+    }
+}
---
+++ ZodiacService/ZodiacService/Operations/ZodiacOperations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ZodiacService.Operations
+{
+    public class ZodiacOperations
+    {
+        private const string FilePath = "./zodiacsigns.txt";
+
+        public static List<Tuple<Zodiac, Zodiac, string>> GetAllZodiacs()
+        {
+            var zodiacs = new List<Tuple<Zodiac, Zodiac, string>>();
+            try
+            {
+                var sr = new StreamReader(FilePath);
+                var line = sr.ReadLine()?.Split(" ");
+                while (line != null)
+                {
+                    zodiacs.Add(new Tuple<Zodiac, Zodiac, string>(
+                        new Zodiac() { Date = line[0] },
+                        new Zodiac() { Date = line[1] },
+                        line[2]));
+
+                    line = sr.ReadLine()?.Split(" ");
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+            return zodiacs;
+        }
+    }
+}
+
