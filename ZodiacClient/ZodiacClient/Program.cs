f22d2e3b2247215b43e2e5ac62fa22939b729144
timus09 <andreitimus9@gmail.com>
2021-04-20 03:27:02 +0300

    made function for validate

--- ZodiacClient/ZodiacClient/Program.cs
+++ ZodiacClient/ZodiacClient/Program.cs
@@ -10,6 +10,27 @@ namespace ZodiacClient
 {
     class Program
     {
-        
+        public static bool ValidateDate(string date)
+        {
+
+            var month = int.Parse(date.Substring(0, 2));
+            var day = int.Parse(date.Substring(3, 2));
+            var year = int.Parse(date.Substring(6, 4));
+
+            try
+            {
+                var dateTime = new DateTime(year, month, day);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Invalid Date!");
+                return false;
+            }
+
+            var regex = new Regex("([0-9]{2})/([0-9]{2})/([0-9]{4})");
+            var match = regex.Match(date);
+
+            return match.Success;
+        }
     }
 }
